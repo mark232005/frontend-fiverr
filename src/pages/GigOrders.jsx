9cc7873cbcf9ca5c0a1d9d043155bfce44024e58
DimaAlufCoding <aluf.dimacoding@gmail.com>
2025-06-07 19:18:06 +0300

    order data

--- src/pages/GigOrders.jsx
+++ src/pages/GigOrders.jsx
@@ -5,32 +5,62 @@
 
 import { useEffect, useState } from 'react'
 import { orderService } from '../services/order/order.service.local.js'
+import { gigService } from '../services/gig/gig.service.local.js'
+
 
 export function GigOrders() {
-  const [orders, setOrders] = useState([])
+    const [orders, setOrders] = useState([])
+    const [gigsMap, setGigsMap] = useState({})
 
-  useEffect(() => {
-    loadOrders()
-  }, [])
 
-  async function loadOrders() {
-    const orders = await orderService.query()
-    setOrders(orders)
-  }
+    useEffect(() => {
+        loadOrders()
+    }, [])
 
-  return (
+    async function loadOrders() {
+        const orders = await orderService.query()
+        setOrders(orders)
+
+
+        const gigs = await Promise.all(orders.map(order => gigService.getById(order.gigId)))
+        const map = {}
+        gigs.forEach(gig => {
+            if (gig) map[gig._id] = gig
+        })
+        setGigsMap(map)
+    }
+
+return (
     <section className="order-index">
-      <h2>Your Orders</h2>
-      <ul>
-        {orders.map(order => (
-          <li key={order._id}>
-            <h4>Order for Gig: {order.gigId}</h4>
-            <p>Seller: {order.seller.fullname}</p>
-            <p>Status: {order.status}</p>
-            <p>Price: ${order.price}</p>
-          </li>
-        ))}
-      </ul>
+        <h2>My Order</h2>
+        <ul>
+            {orders.map(order => {
+                const gig = gigsMap[order.gigId] 
+
+                return (
+                    <li key={order._id}>
+                        <div className='order-info'>
+                            <h3>ORDER STATUS</h3>
+                            <p>{order.status}</p>
+                            <p className='date'>
+                                Due date: {order?.dueDate ? new Date(order.dueDate).toLocaleDateString('en-GB', {
+                                    day: 'numeric',
+                                    month: 'short',
+                                    year: 'numeric'
+                                }) : 'No due date'}
+                            </p>
+                        </div>
+                        <div className='gig-order'>
+                            {gig && <img src={gig.imgUrl} alt={gig.title} style={{ width: '100px' }} />}
+                        </div>
+                        <h4>Order for Gig: {gig ? gig.title : order.gigId}</h4>
+                        <p>Seller: {order.seller.fullname}</p>
+                        <p>Price: ${order.price}</p>
+                    </li>
+                )
+            })}
+        </ul>
     </section>
-  )
+)
+
 }
--- src/services/order/data/order-data.js
+++ src/services/order/data/order-data.js
@@ -1,18 +1,20 @@
 export const demoOrders = [
-  {
-    "gigId": "g101",
-    "buyer": { "_id": "u102", "fullname": "John Doe" },
-    "seller": { "_id": "u101", "fullname": "frederickkessie" },
-    "createdAt": Date.now(),
-    "status": "pending",
-    "price": 172
-  },
-  {
-    "gigId": "g102",
-    "buyer": { "_id": "u102", "fullname": "Jane Smith" },
-    "seller": { "_id": "u103", "fullname": "vividstore" },
-    "createdAt": Date.now(),
-    "status": "completed",
-    "price": 151
-  }
+    {
+        "gigId": "g101",
+        "buyer": { "_id": "u102", "fullname": "John Doe" },
+        "seller": { "_id": "u101", "fullname": "frederickkessie" },
+        "createdAt": Date.now(),
+        "dueDate": new Date('2025-06-11').getTime(),
+        "status": "pending",
+        "price": 172
+    },
+    {
+        "gigId": "g102",
+        "buyer": { "_id": "u102", "fullname": "Jane Smith" },
+        "seller": { "_id": "u103", "fullname": "vividstore" },
+        "createdAt": Date.now(),
+        "dueDate": new Date('2025-06-15').getTime(),
+        "status": "completed",
+        "price": 151
+    }
 ]
