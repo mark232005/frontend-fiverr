28978493bf911e74101053608b29e786d2dfb9fa
DimaAlufCoding <aluf.dimacoding@gmail.com>
2025-06-07 19:54:26 +0300

    gig order

--- src/pages/GigOrders.jsx
+++ src/pages/GigOrders.jsx
@@ -11,10 +11,19 @@ import { gigService } from '../services/gig/gig.service.local.js'
 export function GigOrders() {
     const [orders, setOrders] = useState([])
     const [gigsMap, setGigsMap] = useState({})
+    const [orderNumber, setOrderNumber] = useState(null)
+    const [deliveryDays, setDeliveryDays] = useState(null)
+
+
 
 
     useEffect(() => {
         loadOrders()
+        const randomOrderNumber = Math.floor(100000000 + Math.random() * 900000000)
+        setOrderNumber(randomOrderNumber)
+        const randomDays = Math.floor(Math.random() * 14) + 1
+        setDeliveryDays(randomDays)
+
     }, [])
 
     async function loadOrders() {
@@ -30,37 +39,56 @@ export function GigOrders() {
         setGigsMap(map)
     }
 
-return (
-    <section className="order-index">
-        <h2>My Order</h2>
-        <ul>
-            {orders.map(order => {
-                const gig = gigsMap[order.gigId] 
+    return (
+        <section className="order-index">
+            <h2>My Order</h2>
+            <ul>
+                {orders.map(order => {
+                    const gig = gigsMap[order.gigId]
 
-                return (
-                    <li key={order._id}>
-                        <div className='order-info'>
-                            <h3>ORDER STATUS</h3>
-                            <p>{order.status}</p>
-                            <p className='date'>
-                                Due date: {order?.dueDate ? new Date(order.dueDate).toLocaleDateString('en-GB', {
-                                    day: 'numeric',
-                                    month: 'short',
-                                    year: 'numeric'
-                                }) : 'No due date'}
-                            </p>
-                        </div>
-                        <div className='gig-order'>
-                            {gig && <img src={gig.imgUrl} alt={gig.title} style={{ width: '100px' }} />}
-                        </div>
-                        <h4>Order for Gig: {gig ? gig.title : order.gigId}</h4>
-                        <p>Seller: {order.seller.fullname}</p>
-                        <p>Price: ${order.price}</p>
-                    </li>
-                )
-            })}
-        </ul>
-    </section>
-)
+                    return (
+                        <li key={order._id} className="order-preview">
+                            <div className='order-info'>
+                                <h3>ORDER STATUS</h3>
+                                <p>{order.status}</p>
+                                <p className='date'>
+                                    Due date: {order?.dueDate ? new Date(order.dueDate).toLocaleDateString('en-GB', {
+                                        day: 'numeric',
+                                        month: 'short',
+                                        year: 'numeric'
+                                    }) : 'No due date'}
+                                </p>
+                            </div>
+                            <div className='gig-order'>
+                                <div className='order-img'>
+                                    {gig && <img src={gig.imgUrl} alt={gig.title} style={{ width: '100px' }} />}
+                                </div>
+                                <div className='gig-info'>
+                                    <h4>{gig ? gig.title : order.gigId}</h4>
+                                    <h4>Graphics & Design</h4> {/* need to finish this */}
+                                    <p>From: {order.seller.fullname}</p>
+                                </div>
+                                <div className='order-data'>
+                                    <div className='order'>
+                                        <h4>ORDER NO.</h4>
+                                        <span>{orderNumber}</span>
+                                    </div>
+                                    <div className='delivery'>
+                                        <h4>Delivery in:</h4>
+                                        <span>{deliveryDays} days</span>
+                                    </div>
+                                    <div className='order-button'>
+                                        <button>VIEW ORDER</button>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        </li>
+                    )
+                })}
+            </ul>
+        </section>
+    )
 
 }
