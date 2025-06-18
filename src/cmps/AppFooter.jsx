2bab77e7e8f0db0758009e0b7024d5d7a2fa5f3c
mark <alufmark@gmail.com>
2025-06-18 16:01:26 +0300

    Add a footer

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -1,13 +1,37 @@
 import { useSelector } from 'react-redux'
+import { AccessibilityIcon, Facebook, Global, Instagram, InstagramIcon, Linkedin, Pinterest, Tiktok, Twitter } from '../svg'
 
 export function AppFooter() {
     const count = useSelector(storeState => storeState.userModule.count)
 
     return (
-        <footer className="app-footer main-container full">
-            <section>
-                <p>Coffeerights &copy; 2024</p>
-                <p>Count: {count}</p>
+        <footer>
+            <section className="app-footer flex">
+                <div className="flex logo">
+                    <p className="logo-txt">
+                        alufix<span className="dom"></span>
+                    </p>
+                    <p>© Alufix International Ltd. 2025</p>
+                </div>
+                <div className="flex footer-links">
+                    <div className="icons">
+                        <button><Tiktok /></button>
+                        <button><Instagram /></button>
+                        <button><Linkedin /></button>
+                        <button><Facebook /></button>
+                        <button><Pinterest /></button>
+                        <button><Twitter /></button>
+                    </div>
+                    <span>.</span>
+                    <div className="flex language-currency">
+                        <div className="language">
+                            <Global />
+                            <span>English</span>
+                        </div>
+                        <p>₪ ILS</p>
+                    </div>
+                    <AccessibilityIcon />
+                </div>
             </section>
         </footer>
     )
