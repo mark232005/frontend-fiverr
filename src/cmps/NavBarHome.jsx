a53028e8d44a5aab8db09802c84400d3d5c1a3e6
mark <alufmark@gmail.com>
2025-07-02 20:50:32 +0300

    Mq home page

--- src/cmps/NavBarHome.jsx
+++ src/cmps/NavBarHome.jsx
@@ -2,42 +2,60 @@
 export function NavBarHome() {
     return (
         <section className="nav-bar-header">
-                            <button>
+            <button>
+                <div className="img flex">
                     <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/programming-tech-thin.56382a2.svg" alt="" />
-                    <p>Programming & Tech</p>
-                </button>
-                <button>
+                </div>
+                <p>Programming & Tech</p>
+            </button>
+            <button>
+                <div className="img flex">
                     <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/graphics-design-thin.ff38893.svg" alt="" />
-                    <p> Graphics & Design</p>
-                </button>
-                <button>
+                </div>
+                <p> Graphics & Design</p>
+            </button>
+            <button >
+                <div className="img flex">
                     <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/digital-marketing-thin.68edb44.svg" alt="" />
-                    <p>Digital Marketing</p>
-                </button>
-                <button>
+                </div>
+                <p>Digital Marketing</p>
+            </button>
+            <button>
+                <div className="img flex">
                     <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/writing-translation-thin.fd3699b.svg" alt="" />
-                    <p>Writing & Translation</p>
-                </button>
-                <button>
+                </div>
+                <p>Writing & Translation</p>
+            </button>
+            <button>
+                <div className="img flex">
                     <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/video-animation-thin.9d3f24d.svg" alt="" />
-                    <p>Video & Animation</p>
-                </button>
-                <button>
+                </div>
+                <p>Video & Animation</p>
+            </button>
+            <button>
+                <div className="img flex">
                     <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/ai-services-thin.104f389.svg" alt="" />
-                    <p>AI Services</p>
-                </button>
-                <button>
-                    <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/music-audio-thin.43a9801.svg" alt="" />
-                    <p>Music & Audio</p>
-                </button>
-                <button>
-                    <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/business-thin.885e68e.svg" alt="" />
-                    <p>Business</p>
-                </button>
-                <button>
-                    <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/consulting-thin.d5547ff.svg" alt="" />
-                    <p>Consulting</p>
-                </button>
+                </div>
+                <p>AI Services</p>
+            </button>
+            <button>
+                <div className="img flex">
+                <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/music-audio-thin.43a9801.svg" alt="" />
+                </div>
+                <p>Music & Audio</p>
+            </button>
+            <button>
+                <div className="img flex">
+                <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/business-thin.885e68e.svg" alt="" />
+                </div>
+                <p>Business</p>
+            </button>
+            <button>
+                <div className="img flex">
+                <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/consulting-thin.d5547ff.svg" alt="" />
+                </div>
+                <p>Consulting</p>
+            </button>
 
         </section>
     )
